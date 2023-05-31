45920418d50a05c53af43f49de17f02c78762b88
redoccean <vv8834480@gmail.com>
2023-05-31 23:27:13 +0700

    Главное

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const minSpan = document.getElementById("min_price");
 const maxSpan = document.getElementById("max_price");
 
 const finances = [
-  {date: "01.01.1970", price: 103},
-  {date: "02.01.1970", price: 113},
-  {date: "03.01.1970", price: 143},
-  {date: "04.01.1970", price: 83},
-  {date: "05.01.1970", price: 323},
-  {date: "06.01.1970", price: 133},
-  {date: "07.01.1970", price: 185},
-  {date: "08.01.1970", price: 199},
+  {date: "12.07.2022", price: 5.555},
+  {date: "12.07.2021", price: 6.915},
+  {date: "24.07.2020", price: 10.725},
+  {date: "21.06.2019", price: 7.77},
+  {date: "04.01.2019", price: 6.925},
+  {date: "05.10.2018", price: 6.99},
+  {date: "25.05.2017", price: 7.715},
+  {date: "12.09.2016", price: 4.85},
 ];
 let min = finances[0].price;
 let max = finances[0].price;
 for(let i = 0; i < finances.length; i++) {
-  const element = document.createElement("p");
+  const element = document.createElement("tr");
   element.innerHTML = `<p>${finances[i].date}: ${finances[i].price}</p>`;
   table.append(element);
 
